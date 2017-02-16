fb386dbb298d20729ade352b52c4284623d7bd05
chru <christian.ruiz@beezy.net>
2017-02-16 20:40:07 +0100

    fix: Added Main storyboard to the iOS project

---
+++ Conscience/Conscience.Mobile.Hosts.iPhone/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+
+namespace Conscience.Mobile.Hosts.iPhone
+{
+    public class Application
+    {
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
