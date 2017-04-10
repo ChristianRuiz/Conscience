97209386ee9b81bebe9a2286c47d3f135086dd48
chru <christian.ruiz@beezy.net>
2017-04-10 20:27:59 +0200

    Added authorization to the start page

--- Conscience/Conscience.Web/App_Start/Startup.Auth.cs
+++ Conscience/Conscience.Web/App_Start/Startup.Auth.cs
@@ -26,7 +26,7 @@ namespace Conscience.Web
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
+                LoginPath = new PathString("/Login"),
                 Provider = new CookieAuthenticationProvider
                 {
                     // Enables the application to validate the security stamp when the user logs in.
--- Conscience/Conscience.Web/Controllers/HomeController.cs
+++ Conscience/Conscience.Web/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace Conscience.Web.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
