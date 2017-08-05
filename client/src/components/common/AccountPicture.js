3fd8da12ca1696e78cd73b44586d98f49b404173
chru <christian.ruiz@beezy.net>
2017-08-05 11:27:19 +0200

    Added default image

--- client/src/components/common/AccountPicture.js
+++ client/src/components/common/AccountPicture.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AccountPicture = ({ pictureUrl }) =>
   <div>
-    <div className="picture" style={{ backgroundImage: `url(${pictureUrl})` }} />
+    <div className="picture" style={{ backgroundImage: `url('${pictureUrl}'), url('/Content/images/card/defaultProfile.png')` }} />
   </div>;
 
 AccountPicture.propTypes = {
