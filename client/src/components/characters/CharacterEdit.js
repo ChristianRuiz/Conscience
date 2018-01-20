ec2f2e1f70c823bf43c881f90476996d3c4bb644
chru <christian.ruiz@beezy.net>
2018-01-20 16:58:47 +0100

    Characters edit completed

--- client/src/components/characters/CharacterEdit.js
+++ client/src/components/characters/CharacterEdit.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { Redirect } from 'react-router';
 import { graphql, gql, compose } from 'react-apollo';
 
 import TextField from 'material-ui/TextField';
+import AutoComplete from 'material-ui/AutoComplete';
 
-import PictureDescriptionBox from '../common/PictureDescriptionBox';
+import AccountPicture from '../common/AccountPicture';
 
 import query from '../../queries/CharacterDetailQuery';
 
@@ -24,12 +25,16 @@ class CharacterEdit extends React.Component {
       triggers: [],
       memories: [],
       plots: [],
-      relations: []
+      relations: [],
+      autocompletePlots: [],
+      autocompleteCharacters: []
     };
+    this.plotsCount = -1;
+    this.relationsCount = -1;
   }
 
   componentWillReceiveProps(props) {
-    if (!props.data.loading && !this.state.editing) {
+    if (!props.data.loading && !props.autocomplete.loading && !this.state.editing) {
       if (props.data && props.data.characters && props.data.characters.byId) {
         const character = props.data.characters.byId;
 
@@ -75,6 +80,18 @@ class CharacterEdit extends React.Component {
       stateToSet.memories = [...state.memories, { description: '' }];
     }
 
+    if (!props.autocomplete.loading && (this.plotsCount < 0 || this.plotsCount !== state.plots.length)) {
+      this.plotsCount = state.plots.length;
+      stateToSet.autocompletePlots = props.autocomplete.plots.all.filter(p =>
+        state.plots.filter(sp => sp.plot.id === p.id).length === 0);
+    }
+
+    if (!props.autocomplete.loading && (this.relationsCount < 0 || this.relationsCount !== state.relations.length)) {
+      this.relationsCount = state.relations.length;
+      stateToSet.autocompleteCharacters = props.autocomplete.characters.all.filter(c =>
+        state.relations.filter(r => r.character.id === c.id).length === 0);
+    }
+
     if (Object.keys(stateToSet).length !== 0) {
       this.setState(stateToSet);
     }
@@ -102,12 +119,42 @@ class CharacterEdit extends React.Component {
           }))
         }
       }
-    }).then(() => this.setState({ edited: true }));
+    }).then(r => this.setState({ edited: true, characterId: r.data.characters.addOrModifyCharacter.id }));
+  }
+
+  replaceArrayPosition(array, i, value) {
+    const newArray = [...array];
+    newArray[i] = value;
+    return newArray;
+  }
+
+  plotSelected(plot, i) {
+    if (i !== -1) {
+      this.autocompletePlots.setState({ searchText: '' });
+
+      this.setState({ plots: [...this.state.plots, {
+        plot,
+        description: ''
+      }]
+      });
+    }
+  }
+
+  characterSelected(character, i) {
+    if (i !== -1) {
+      this.autocompleteCharacters.setState({ searchText: '' });
+
+      this.setState({ relations: [...this.state.relations, {
+        character,
+        description: ''
+      }]
+      });
+    }
   }
 
   render() {
     if (this.state.edited) {
-      return <Redirect to="/characters" />;
+      return <Redirect to={`/character-detail/${this.state.characterId}`} />;
     }
 
     if (!this.state.editing) {
@@ -127,46 +174,102 @@ class CharacterEdit extends React.Component {
       </div>
       <div>
         <h2>Character Story:</h2>
-        <p>{this.state.story}</p>
+        <TextField
+          multiLine
+          rows={4}
+          fullWidth
+          hintText="Story"
+          value={this.state.story}
+          onChange={e => this.setState({ story: e.target.value })}
+        />
       </div>
 
       <h2>Triggers:</h2>
 
       <ul>
         {this.state.triggers.map((trigger, i) =>
-          <li key={i}>{trigger.description}</li>)}
+          <li key={i}>
+            <TextField
+              name={`trigger${i}`}
+              fullWidth
+              value={trigger.description}
+              onChange={e => this.setState({ triggers: this.replaceArrayPosition(this.state.triggers, i, { id: trigger.id, description: e.target.value }) })}
+            />
+          </li>)}
       </ul>
 
       <h2>Memories:</h2>
 
       <ul>
         {this.state.memories.map((memory, i) =>
-          <li key={i}>{memory.description}</li>)}
+          <li key={i}>
+            <TextField
+              name={`memory${i}`}
+              fullWidth
+              value={memory.description}
+              onChange={e => this.setState({ memories: this.replaceArrayPosition(this.state.memories, i, { id: memory.id, description: e.target.value }) })}
+            />
+          </li>)}
       </ul>
 
       <h2>Plots:</h2>
 
+      <AutoComplete
+        floatingLabelText="Add a new plot"
+        menuStyle={{ backgroundColor: 'black' }}
+        filter={AutoComplete.caseInsensitiveFilter}
+        dataSource={this.state.autocompletePlots}
+        dataSourceConfig={{ text: 'name', value: 'id' }}
+        ref={ref => this.autocompletePlots = ref}
+        onNewRequest={(data, i) => this.plotSelected(data, i)}
+      />
+
       <ul>
-        {this.state.plots.map(p =>
+        {this.state.plots.map((p, i) =>
           <li key={p.plot.id}>
-            <p><Link to={`/plot-detail/${p.plot.id}`} >{p.plot.name}</Link>: {p.plot.description}</p>
-            <p><b>Character involvement: </b></p>
-            <p>{p.description}</p>
+            <p><b>{p.plot.name}:</b> {p.plot.description}</p>
+            <TextField
+              floatingLabelText="Character involvement"
+              name={`plot${p.plot.id}`}
+              multiLine
+              rows={2}
+              fullWidth
+              value={p.description}
+              onChange={e => this.setState({ plots: this.replaceArrayPosition(this.state.plots, i, Object.assign(p, { description: e.target.value })) })}
+            />
           </li>)}
       </ul>
 
       <div>
         <h2>Relations:</h2>
 
+        <AutoComplete
+          floatingLabelText="Add a new relation"
+          menuStyle={{ backgroundColor: 'black' }}
+          filter={AutoComplete.caseInsensitiveFilter}
+          dataSource={this.state.autocompleteCharacters}
+          dataSourceConfig={{ text: 'name', value: 'id' }}
+          ref={ref => this.autocompleteCharacters = ref}
+          onNewRequest={(data, i) => this.characterSelected(data, i)}
+        />
+
         <div className="flexColumn marginTop">
-          {this.state.relations.map(relation =>
-            <PictureDescriptionBox
-              key={relation.character.id}
-              pictureUrl={relation.character.currentHost ? relation.character.currentHost.host.account.pictureUrl : ''}
-              title={relation.character.name}
-              link={`/character-detail/${relation.character.id}`}
-              description={relation.description}
-            />)}
+          {this.state.relations.map((relation, i) =>
+            <div className="pictureDescriptionBox" key={relation.character.id}>
+              <AccountPicture pictureUrl={relation.character.currentHost ? relation.character.currentHost.host.account.pictureUrl : ''} />
+              <div className="titleDescription">
+                <h1>{relation.character.name}</h1>
+                <TextField
+                  floatingLabelText="Characters relation"
+                  name={`relation${relation.character.id}`}
+                  multiLine
+                  rows={2}
+                  fullWidth
+                  value={relation.description}
+                  onChange={e => this.setState({ relations: this.replaceArrayPosition(this.state.relations, i, Object.assign(relation, { description: e.target.value })) })}
+                />
+              </div>
+            </div>)}
         </div>
       </div>
     </div>);
@@ -175,6 +278,7 @@ class CharacterEdit extends React.Component {
 
 CharacterEdit.propTypes = {
   data: React.PropTypes.object.isRequired,
+  autocomplete: React.PropTypes.object.isRequired,
   mutate: React.PropTypes.func.isRequired
 };
 
@@ -215,4 +319,21 @@ mutation AddOrModifyCharacter($character:CharacterInput!) {
 }
       `;
 
-export default withRouter(compose(graphql(query), graphql(mutation))(CharacterEdit));
+const autocomplete = gql`
+query CharacterEditAutocomplete {
+  plots {
+    all {
+      id
+      name
+    }
+  }
+  characters {
+    all {
+      id
+      name
+    }
+  }
+}
+      `;
+
+export default withRouter(compose(graphql(query), graphql(mutation), graphql(autocomplete, { name: 'autocomplete' }))(CharacterEdit));
